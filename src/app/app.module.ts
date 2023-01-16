ef50ec96081dd60e80c0b1858f89cdf6f0efd91b
AgustinSW323 <aguolmedo3@gmail.com>
2023-01-16 17:45:31 -0300

    Angular adapted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent,
+    EducationComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
